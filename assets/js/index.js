78e7f0b3b106075294b2a97dbffa94f89dc48470
david <telegd.david12@gmail.com>
2024-06-25 07:28:59 +0300

    navbar animation ready

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,16 @@
-console.log('teszt');
-
 let navButton = document.getElementById("navButton");
 let navContainer = document.getElementById("navContainer");
+let innerCircle = document.getElementById("innerCircle");
+let outerCircle = document.getElementById("outerCircle");
+
+let modes = ["production", "development"];
+let mode = modes[0];
+console.log(mode);
 
 navButton.addEventListener("click", function(){
-    gsap.to(navContainer, 0.3, {display:'block'});
-});
+    let durationMultiplier = mode === "development" ? 10 : 1;
+
+    gsap.fromTo(navContainer,{width:"0%", height:"0%"},{width:"100vw",height:"70vh",display:"block"});
+    gsap.fromTo(innerCircle,{width:"0%", height:"0%"},{width:"100%", height:"100%", duration:0.6 * durationMultiplier});
+    gsap.fromTo(outerCircle,{width:"0%", height:"0%"},{width:"100%", height:"100%", duration:0.5 * durationMultiplier});   
+}); 
